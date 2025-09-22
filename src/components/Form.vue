2e14a1533177e21f5bee4c053ed6d0444a26347f
ForgeSolucoesEstagio <juniorfranca632@gmail.com>
2025-09-22 11:38:04 -0300

    Formulário inicial

---
+++ src/components/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div class="panel">
+      <form class="form">
+        <div class="form-header">
+          <p>To Do List</p>
+        </div>
+        
+        <div class="form-group">
+          <label class="form-label">Tarefa</label>
+          <input 
+            type="text" 
+            class="form-input" 
+            placeholder="Digite uma tarefa..."
+          >
+        </div>
+        
+        <button type="submit" class="btn">
+          Adicionar
+        </button>
+      </form>
+    </div>
+
+    <section>
+    </section>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
